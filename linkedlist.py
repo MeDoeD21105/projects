c324f6e0e5e8df323d1ae33f5636cafaee640082
MeDoeD です <medoed21105@gmail.com>
2024-03-23 17:01:48 +0300

    added linked list

---
+++ linkedlist.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return f"[{self.data}]->{self.next} Node"
+
+class Linkedlist:
+    def __init__(self, data=None):
+        self.head = None
+
+    def append(self, data):
+        if self.head is None:
+            self.head = Node(data)
+            return data
+        node = self.head
+        while node.next:
+            node = node.next
+        node.next = Node(data)
+
+    def __str__(self):
+        return f"{self.head} ->Linked"
+
+
+kl = Linkedlist()
+kl.append(1)
+kl.append(2)
+kl.append(10)
+kl.append(20)
+kl.append(30)
+print(kl)
