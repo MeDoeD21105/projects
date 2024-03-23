cabb967c79b713dbb7a1ea27da4d376e841b2ad2
MeDoeD です <medoed21105@gmail.com>
2024-03-23 19:12:26 +0300

    good parsing youtube, с небольшими памарками)))

---
+++ parser_name_video_django/parser_name_video_django.py
@@ -0,0 +1,28 @@
+import requests
+import chardet
+from bs4 import BeautifulSoup
+
+
+#url = ("https://www.youtube.com/playlist?list=PLA0M1Bcd0w8yU5h2vwZ4LO7h1xt8COUXl")
+#headers = {
+#    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+#}
+#req = requests.get(url, headers=headers)
+#src = req.text
+
+
+#encoding = chardet.detect(src.encode())['encoding']
+#with open("index.html", "w") as file:
+#    file.write(src)
+
+with open("index.html") as file:
+    src = file.read()
+
+
+soup = BeautifulSoup(src, "lxml")
+link = soup.find_all(class_="yt-simple-endpoint style-scope ytd-playlist-video-renderer")
+#for i in link:
+#    print(i.text)
+with open("django_learning_topics.txt", "w") as file:
+    for i in link:
+        file.write(i.text)
