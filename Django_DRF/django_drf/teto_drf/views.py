4e47049dbaa7bf180595516b90a4f07d793e4f94
MeDoeD です <medoed21105@gmail.com>
2024-04-14 12:30:19 +0300

    male modif

--- Django_DRF/django_drf/teto_drf/views.py
+++ Django_DRF/django_drf/teto_drf/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .serializers import TetoSerializer
 
 from .models import *
+
+
+class TetoAPIView(APIView):
+    def get(self, request):
+        lst = Teto.objects.all().values()
+        return Response({"posts": list(lst)})
+
 # Create your views here.
-class TetoAPIView(generics.ListAPIView):
-    queryset = Teto.objects.all()
-    serializer_class = TetoSerializer
+#class TetoAPIView(generics.ListAPIView):
+#    queryset = Teto.objects.all()
+#    serializer_class = TetoSerializer
