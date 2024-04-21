24581ff602a5887d27b917af3a1a519cc103b50a
MeDoeD です <medoed21105@gmail.com>
2024-04-21 21:55:57 +0300

    added linked list full

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -1,52 +1,78 @@
 class Node:
-    def __init__(self, data = None):
+    def __init__(self, data):
         self.data = data
         self.next = None
 
-    def __str__(self):
-        return f"[{self.data}]->{self.next} Node"
 
-class Linkedlist:
+class LinkedList:
     def __init__(self):
         self.head = None
+        self.num_of_nodes = 0
+
+    def prepend(self, data):
+        self.num_of_nodes += 1
+        node = Node(data)
+
+        if self.head:
+            node.next = self.head
+            self.head = node
+        else:
+            self.head = node
+
     def append(self, data):
-        if self.head is None:
-            self.head=Node(data)
-            return data
-        node = self.head
-        
-        while node.next:
-            node = node.next
-            
-        node.next = Node(data)
-        
-    def length(self):
-        count = 0
-        node = self.head
-        
-        while node:
-            count += 1
-            node = node.next
-            
-        return count
+        self.num_of_nodes += 1
+        node = Node(data)
 
-    def elem(self):
-        elements = []
-        node = self.head
-        while node:
-            elements.append(node.data)
-            node = node.next
-        print(elements)
-        
-    def __str__(self):
-        return f"{self.head} ->Linked"
+        current_node = self.head
 
-kl = Linkedlist()
-kl.append(1)
-kl.append(2)
-kl.append(10)
-kl.append(20)
-kl.append(30)
-print(kl)
-print(kl.length())
-kl.elem()
+        while current_node.next:
+            current_node = current_node.next
+
+        current_node.next = node
+
+    def remove(self, data):
+        if not self.head:
+            return
+
+        current_node = self.head
+        prev_node = None
+
+        while current_node and current_node.data != data:
+            prev_node = current_node
+            current_node = current_node.next
+
+        if not current_node:
+            return
+
+        if prev_node:
+            prev_node.next = current_node.next
+        else:
+            self.head = current_node.next
+
+
+        def traverse(self):
+            current_node = self.head
+
+            while current_node:
+                print(f"{current_node.data} --> ", end='')
+                current_node = current_node.next
+
+            print('None')
+
+    def __len__(self):
+        return self.num_of_nodes
+
+    
+
+linked_list = LinkedList()
+linked_list.prepend(4)
+linked_list.prepend('NaN')
+linked_list.prepend('Integer')
+linked_list.prepend('String')
+linked_list.prepend(10)
+linked_list.append(5)
+
+
+linked_list.traverse()
+print(len(linked_list))
+
