5d7e868cf28fd6f0e97d0ec36a55c002933c96db
MeDoeD です <medoed21105@gmail.com>
2024-06-12 01:37:13 +0300

    vsego po чуть-чуть

--- asyncIO/main.py
+++ asyncIO/main.py
@@ -1,14 +1,23 @@
 import asyncio
+import time
 
-async def hello():
-    print("Hello")
+
+async def cor1(start):
+    print(f"ДО СНА КОР1 {time.time() - start:0.0f}")
     await asyncio.sleep(1)
-    print("World")
+    print(f"Posle sleep COR2 {time.time() - start:0.0f}")
+
+
+async def cor2(start):
+    print(f"ДО СНА КОР2 {time.time() - start:0.0f}")
+    await asyncio.sleep(1)
+    print(f"Posle sleep COR2 {time.time() - start:0.0f}")
+
 
 async def main():
-    task1 = asyncio.create_task(hello())
-    task2 = asyncio.create_task(hello())
-    await task1
-    await task2
+    start = time.time()
+    await asyncio.gather(cor1(start), cor2(start))
+    print(f"Pose FINISHED Main {time.time() - start:0.00f}")
+
 
 asyncio.run(main())
