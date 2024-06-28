81cd679c401c3a2dd01c5563a747f688b0fe3b02
MeDoeD です <medoed21105@gmail.com>
2024-06-28 22:47:04 +0300

    male modif

--- Django_DRF/django_drf/teto_drf/views.py
+++ Django_DRF/django_drf/teto_drf/views.py
@@ -1,14 +1,29 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import *
 from .serializers import TetoSerializers
+from rest_framework.decorators import action
 
 
-# 7
+# 9
 
 class TetoViewSet(viewsets.ModelViewSet):
     queryset = Teto.objects.all()
     serializer_class = TetoSerializers
+    
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+
+        if not pk:
+            return Teto.objects.all()
+
+        return Teto.objects.filter(pk=pk)
+    
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name + " Baka Jopa"})
 
 # class TetoAPIList(generics.ListCreateAPIView):
 #     queryset = Teto.objects.all()
